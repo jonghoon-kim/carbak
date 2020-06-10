abdc9606498be2633ddfdcc99ca218bcfe55be51
kshkim624 <kshkim624@naver.com>
2020-06-10 16:46:30 +0900

    .

--- chabak/src/main/java/com/chabak/vo/Reply.java
+++ chabak/src/main/java/com/chabak/vo/Reply.java
@@ -1,4 +1,19 @@
 package com.chabak.vo;
 
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class Reply {
+    private int replyNo;
+    private String id;
+    private String content;
+    private Date regDate;
+    private Date modifyDate;
+    private int reviewNo;
+    private int groupNo;
+    private int groupOrder;
+    private int lv;
+
 }
