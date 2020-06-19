8121ca955c77108d3732cce66bc0b519f0346c51
kshkim624 <kshkim624@naver.com>
2020-06-19 13:39:17 +0900

    .

--- chabak/src/main/java/com/chabak/controllers/ReviewController.java
+++ chabak/src/main/java/com/chabak/controllers/ReviewController.java
@@ -19,6 +19,8 @@ public class ReviewController {
 
     @Autowired
     ReviewDao reviewDao;
+
+    @Autowired
     ReplyDao replyDao;
 
     @RequestMapping(value ={"", "/", "/list"}, method=RequestMethod.GET)
