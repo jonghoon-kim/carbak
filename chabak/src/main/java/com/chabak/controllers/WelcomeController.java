4c812168663fcf477144a0e22ed0829aa9f13e12
kshkim624 <kshkim624@naver.com>
2020-07-10 15:00:36 +0900

    index 작성중...

--- chabak/src/main/java/com/chabak/controllers/WelcomeController.java
+++ chabak/src/main/java/com/chabak/controllers/WelcomeController.java
@@ -1,44 +1,58 @@
 package com.chabak.controllers;
 
 
-import com.chabak.services.MemberService;
-
+import com.chabak.services.ReviewService;
+import com.chabak.vo.ReviewAndLike;
 import lombok.SneakyThrows;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 
 import javax.servlet.http.HttpSession;
-
+import java.util.List;
 
 @Controller
 public class WelcomeController {
+
     @Autowired
-    MemberService memberService;
+    ReviewService reviewService;
 
-
-    //    @SneakyThrows
-//    private Connection getConnection() {
-//        String url =
-//                "jdbc:oracle:thin:@localhost:1521:xe";
-//        String user = "hr2";
-//        String password = "1";
-//        Class.forName("oracle.jdbc.driver.OracleDriver");
-//        return DriverManager.getConnection(url,user,password);
-//
-//
-//    }
     @SneakyThrows
-    @RequestMapping(value="/")
-    public String upload(HttpSession session){
+    @RequestMapping(value= {"", "/", "index"})
+    public ModelAndView index(HttpSession session){
+        ModelAndView mv = new ModelAndView();
         System.out.println("Controller");
 
-        //session.setAttribute("id","fakeId");
-       session.setAttribute("id","id1");
-        System.out.println((String)(session.getAttribute("id")));
+//        session.setAttribute("id","fakeId");
+        //session.setAttribute("id","id1");
+        String id = (String)session.getAttribute("id");
+        List<ReviewAndLike> reviewList = null;
+        if(id==null){
+            reviewList = reviewService.selectReviewTop5(null);
+        }
+        System.out.println("index top5 list(before):"+reviewList);
+        //TODO:나중에 지우기
+        reviewList = reviewService.selectReviewTop5(null);
+        for(ReviewAndLike review:reviewList){
+            String modifiedContent = reviewService.deleteImgTag(review.getContent());
+            review.setContent(modifiedContent);
+        }
 
-        return "index";
+        System.out.println("index top5 list(after):"+reviewList);
+
+        mv.setViewName("/index");
+        mv.addObject("reviewList",reviewList);
+        return mv;
     }
+
+    @RequestMapping("/header")
+    public String header() {
+        return "header";
+    }
+
+    @RequestMapping("/footer")
+    public String footer() {return "footer";}
 }
